b06799f1dad8db33f840757f6b31fae280700213
cad <catx1726@foxmail.com>
2020-05-09 14:57:42 +0800

    test cors

---
+++ code/js/index.js
@@ -0,0 +1,14 @@
+function imgLoad() {
+    // 1. 获取到 BING 的日推图片
+    //'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', // 此接口需要用 nginx处理跨域请求
+    // 备用 'https://api.berryapi.net/?service=App.Bing.Images'
+    let img = fetch(
+        'http://bing.adoba.site:82/proxy/bing/HPImageArchive.aspx?format=js&idx=0&n=1'
+    ).then((res) => {
+        console.log(res)
+    })
+}
+imgLoad()
+// 2. 调用放大图片接口
+// 3. 保存图片到GitHub
+// 4. 24H循环整个流程(附带检测如果有重名的就不放大，也不保存)
