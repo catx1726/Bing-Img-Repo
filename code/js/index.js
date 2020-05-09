b16a1305f457824235e1447adb1687163cbc045f
cad <catx1726@foxmail.com>
2020-05-09 15:56:56 +0800

    nginx 代理请求之后 使用fetch获取到 bing img 完

--- code/js/index.js
+++ code/js/index.js
@@ -2,11 +2,23 @@ function imgLoad() {
     // 1. 获取到 BING 的日推图片
     //'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', // 此接口需要用 nginx处理跨域请求
     // 备用 'https://api.berryapi.net/?service=App.Bing.Images'
-    let img = fetch(
+    const DOC = document
+    let pureImgSrc = ''
+    let _d = new Date()
+    let time = `${_d.getFullYear()}年${_d.getMonth() + 1}月${_d.getDate()}`
+    fetch(
         'https://bing.adoba.site/proxy/bing/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-    ).then((res) => {
-        console.log(res)
-    })
+    )
+        .then((res) => res.json())
+        .then((jsonRes) => {
+            pureImgSrc = 'https://cn.bing.com/' + jsonRes.images[0].url
+            DOC.querySelector('#bing-img-container').src = pureImgSrc
+            console.log(
+                `fetch ${time} bing-img jsonRes:`,
+                jsonRes,
+                ` and img-url:${pureImgSrc}`
+            )
+        })
 }
 imgLoad()
 // 2. 调用放大图片接口
