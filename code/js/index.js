57dc8b98d91f69f23cba5091c592fbeedcceefc9
cad <catx1726@foxmail.com>
2020-05-14 18:16:55 +0800

    脑图完善

--- code/js/index.js
+++ code/js/index.js
@@ -7,8 +7,9 @@ let imgDay = 0,
 
 let pureImgSrc = ''
 
+// 1. 获取到一周的图片
 function imgLoad() {
-    // 1. 获取到 BING 的日推图片
+    // ~~ 1. 获取到 BING 的日推图片 ~~
     //'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', // 此接口需要用 nginx处理跨域请求
     // 备用 'https://api.berryapi.net/?service=App.Bing.Images'
     fetch(
@@ -32,57 +33,35 @@ function imgLoad() {
 }
 imgLoad()
 
-function checkUpdate() {
-    let tempDate = new Date(),
-        nowMonth = tempDate.getMonth() + 1,
-        nowDay = tempDate.getDate(),
-        nowYear = tempDate.getFullYear()
+// function checkUpdate() {
+//     let tempDate = new Date(),
+//         nowMonth = tempDate.getMonth() + 1,
+//         nowDay = tempDate.getDate(),
+//         nowYear = tempDate.getFullYear()
 
-    if (nowMonth !== imgMonth || nowYear !== imgYear) {
-        // 年月份跨度，更新
-        console.log('year / month changed ')
-        imgLoad()
-        return { message: 'year / month 图片更新啦' }
-    }
-    if (nowDay !== imgDay) {
-        // 更新
-        console.log('day changed')
-        imgLoad()
-        return { message: 'day 图片更新啦' }
-    }
-    // 没更新，静默
-    return false
-}
-// 十分钟检测一次 BING 是否更新了图片
-setInterval('checkUpdate()', 10 * 60 * 1000)
-
-// 2. 保存原始图片到GitHub
-// function beforeDown() {
-//     console.log('down')
-//     fetch('https://imgchr.com/json', {
-//         credentials: 'include',
-//         headers: {
-//             'User-Agent':
-//                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
-//             Accept: 'application/json',
-//             'Accept-Language':
-//                 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-//             'Content-Type':
-//                 'multipart/form-data; boundary=---------------------------283715647122960015543343553279',
-//         },
-//         body: `https:${pureImgSrc}`,
-//         method: 'POST',
-//         mode: 'cors',
-//     })
-//         .then((res) => res.json())
-//         .then((resJson) => console.log(resJson))
+//     if (nowMonth !== imgMonth || nowYear !== imgYear) {
+//         // 年月份跨度，更新
+//         console.log('year / month changed ')
+//         imgLoad()
+//         return { message: 'year / month 图片更新啦' }
+//     }
+//     if (nowDay !== imgDay) {
+//         // 更新
+//         console.log('day changed')
+//         imgLoad()
+//         return { message: 'day 图片更新啦' }
+//     }
+//     // 没更新，静默
+//     return false
 // }
-// beforeDown()
+// 十分钟检测一次 BING 是否更新了图片
+// setInterval('checkUpdate()', 10 * 60 * 1000)
+
 // 3. 调用放大图片接口
 // 3.1 保存放大图片到GitHub
 // 4. 24H循环整个流程(附带检测如果有重名的就不放大，也不保存)
 
-// 获取到一周的图片
+// 基本交互
 let imgs = DOC.querySelectorAll('.img-item'),
     slideContainer = DOC.querySelector('.slide-container'),
     prev = DOC.querySelector('.prev'),
