d17b1669663ad0fd4f2cb16153f6aee9ec64e851
cad <catx1726@foxmail.com>
2020-05-14 11:57:52 +0800

    服务器 保存图片/git配置/提交到GitHub/定时任务

---
+++ code/js/github.js
@@ -0,0 +1,47 @@
+const childProcess = require('child_process')
+const cmd = (c) => c
+const shell = 'bash'
+
+const _d = new Date(),
+    year = _d.getFullYear(),
+    month = _d.getMonth() + 1
+
+const config = {
+    env: {
+        NODE_ENV: 'production',
+        encoding: 'utf8',
+    },
+    shell,
+}
+
+const exec = (c) => {
+    return new Promise((resolve, reject) => {
+        childProcess.exec(c, config, (err, stdout, stderr) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(stdout, stderr)
+            }
+        })
+    })
+}
+
+const github = () => {
+    console.log('Depoly start.')
+    exec(cmd('git status'))
+        .then((stdout, stderr) => console.log(stdout, stderr))
+        .then(() => exec(cmd('git add .')))
+        .then(() =>
+            exec(
+                cmd(`git commit -m '${year + '年' + month + '月,Bing壁纸更新'}`)
+            )
+        )
+        .then(() => exec(cmd('git push -u origin master')))
+        .then(() => exec(cmd('Depoly done.')))
+        .catch((err) => {
+            console.log(err)
+            exec(cmd('git checkout :'))
+        })
+}
+
+module.exports = github
---
+++ code/js/main.js
@@ -0,0 +1,51 @@
+/* 
+    DES 丢到服务器上的代码
+    1. 拉取 Bing 请求，保存图片到服务器
+    2. push 服务器端的图片到 GitHub
+    3. 创建 express服务 响应用户请求，返回一周图片列表
+*/
+
+const request = require('request')
+const fs = require('fs')
+const path = require('path')
+const github = require('./github')
+
+const getImg = () => {
+    request.get(
+        'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN',
+        (err, res, body) => {
+            const img = JSON.parse(body).images[0],
+                url = img.url,
+                name = img.copyright,
+                year = img.enddate.slice(0, 4),
+                month = img.enddate.slice(4, 6)
+
+            request(`http://cn.bing.com${url}`).pipe(
+                fs
+                    .createWriteStream(
+                        path.join(`./imgs/bing/${year}/${month}`, name)
+                    )
+                    .on('close', () => {
+                        console.log(`${name} done !`)
+                        github()
+                    })
+            )
+        }
+    )
+}
+
+// 导出后，24H检测一次Bing是否更新壁纸
+module.exports = getImg
+
+/* 定时任务 */
+const schedule = require('node-schedule')
+// const getImg = require('./main')
+
+const rule = new schedule.RecurrenceRule()
+rule.dayOfWeek = [0, new schedule.Range(1, 6)]
+rule.hour = 8
+rule.minute = 0
+
+schedule.scheduleJob(rule, () => {
+    getImg()
+})
