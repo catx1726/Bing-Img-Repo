aa5335644ee4da15a18be247f3f1fa22d6d1ae33
cad <catx1726@foxmail.com>
2020-05-14 16:16:51 +0800

    express 完善

--- express.js
+++ express.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = '8088'
 
-app.get('/public', (req, res) => {
-    res.sendFile(__dirname + '/imgs/bing/2020/05')
-    console.log('Request for ' + req.url + ' received.')
+app.get('/public/bing/:year/:month/', (req, res) => {
+    // DES 这个请求应该接受 年月的参数，然后去响应文件夹下获取所有jpg文件，返回一个url数组
+    try {
+        let list = fs.readdirSync(
+            `./imgs/bing/${req.params['year']}/${req.params['month']}`
+        )
+        res.send(list)
+        console.log(
+            `Request for ${req.url} received. year:${req.params['year']},month:${req.params['month']}`
+        )
+    } catch (error) {
+        console.log(error)
+        res.send('sorry')
+    }
 })
 
 const server = app.listen(port, () => {
