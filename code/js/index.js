6e5511dba1d891322f0d7b206352aac0af15059c
catx1726 <catx1726@foxmail.com>
2020-05-15 17:38:45 +0800

    完善获取服务器上存储的图片列表且去bing请求

--- code/js/index.js
+++ code/js/index.js
@@ -1,6 +1,18 @@
 const DOC = document
 const _d = new Date()
 
+// 基本交互的一些数据
+let imgs = '',
+    slideContainer = DOC.querySelector('.slide-container'),
+    prev = DOC.querySelector('.prev'),
+    next = DOC.querySelector('.next'),
+    downloadBtn = DOC.querySelector('.down-button'),
+    downTrigger = DOC.querySelector('.down-trigger'),
+    // imgDes = DOC.querySelector('.img-else-container'),
+    imgLen = 0, // imgs 总数量
+    nowNum = 0, // 当前第几张
+    imgClick = false
+
 // 获取当前 year / month 用于请求使用
 let nowYear = _d.getFullYear(),
     nowMonth = _d.getMonth() + 1
@@ -13,53 +25,49 @@ let imgDay = 0,
     imgMonth = 0,
     imgYear = 0
 
-let pureImgSrc = ''
+// 请求返回的地址数组: OHR.NorthRimOpens_ZH-CN9513300299.jpg
+// https://cn.bing.com/th?id=OHR.NorthRimOpens_ZH-CN9513300299_1920x1080.jpg
+let fetchTemp = 'https://cn.bing.com/th?id=',
+    size = '_1920x1080.jpg'
+
+// 标准 src 数组
+let imgSrcList = []
 
 // 1. 获取到一周的图片
 function imgLoad() {
     let urlList = []
     fetch(`https://test.dev.adoba.site/public/bing/${nowYear}/${nowMonth}/`)
         .then((res) => {
-            console.log('原始数据：', res)
+            // console.log('原始数据：', res)
             return res.json()
         })
         .then((resJson) => {
-            console.log('json 序列化后的数据：', resJson)
+            resJson.forEach((i) => {
+                let idx = i.lastIndexOf('.jpg')
+                imgSrcList.push(fetchTemp + i.slice(0, idx) + size)
+            })
+            console.log('符合 bing 请求的地址:', imgSrcList)
         })
-
-    // ~~ 1. 获取到 BING 的日推图片 ~~
-    //'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', // 此接口需要用 nginx处理跨域请求
-    // 备用 'https://api.berryapi.net/?service=App.Bing.Images'
-    // fetch(
-    //     'https://bing.adoba.site/proxy/bing/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
-    // )
-    //     .then((res) => res.json())
-    //     .then((jsonRes) => {
-    //         let img = jsonRes.images[0]
-    //         pureImgSrc = 'https://bing.adoba.site/proxy/bing' + img.url
-    //         imgDay = _d.getDate(img.enddate)
-    //         imgMonth = _d.getMonth(img.enddate) + 1
-    //         imgYear = _d.getFullYear(img.enddate)
-    //         let time = `${imgYear}年${imgMonth}月${imgDay}`
-    //         console.log(
-    //             `fetch ${time} bing-img jsonRes:`,
-    //             jsonRes,
-    //             ` and img-url:${pureImgSrc} and imgDay:${imgDay} and imgMonth:${imgMonth} and imgYear:${imgYear}`
-    //         )
-    //         DOC.querySelector('#bing-img-container').src = pureImgSrc
-    //     })
+        .then(() => {
+            createImg()
+            download(0)
+        })
 }
 imgLoad()
 
-// 基本交互
-let imgs = DOC.querySelectorAll('.img-item'),
-    slideContainer = DOC.querySelector('.slide-container'),
-    prev = DOC.querySelector('.prev'),
-    next = DOC.querySelector('.next'),
-    imgDes = DOC.querySelector('.img-else-container'),
-    imgLen = imgs.length,
-    nowNum = 0,
-    imgClick = false
+// 2. 创建 img 元素
+function createImg() {
+    const fragment = DOC.createDocumentFragment()
+    imgSrcList.forEach((i) => {
+        const img = DOC.createElement('img')
+        img.src = i
+        img.classList = 'img-item'
+        fragment.appendChild(img)
+    })
+    slideContainer.appendChild(fragment)
+    imgs = DOC.querySelectorAll('.img-item')
+    imgLen = imgs.length
+}
 
 next.addEventListener('click', nextImg, false)
 prev.addEventListener('click', prevImg, false)
@@ -70,51 +78,26 @@ function showDetail() {
     imgClick = !imgClick
     // 隐藏
     if (imgClick) {
-        prev.style.opacity = '0'
-        next.style.opacity = '0'
-        imgDes.style.opacity = '0'
+        downTrigger.style.opacity = '0'
     } else {
         // 显示
-        prev.style.opacity = '.1'
-        next.style.opacity = '.1'
-        imgDes.style.opacity = '1'
+        downTrigger.style.opacity = '1'
     }
 }
+
+function download(idx) {
+    downloadBtn.href = imgSrcList[idx]
+}
+
 function nextImg() {
     nowNum < imgLen - 1 ? nowNum++ : false
+    download(nowNum)
     let temp = -100 * nowNum + 'vw'
     slideContainer.style.transform = `translateX(${temp})`
 }
 function prevImg() {
     nowNum ? nowNum-- : false
+    download(nowNum)
     let temp = -100 * nowNum + 'vw'
     slideContainer.style.transform = `translateX(${temp})`
 }
-
-// function checkUpdate() {
-//     let tempDate = new Date(),
-//         nowMonth = tempDate.getMonth() + 1,
-//         nowDay = tempDate.getDate(),
-//         nowYear = tempDate.getFullYear()
-
-//     if (nowMonth !== imgMonth || nowYear !== imgYear) {
-//         // 年月份跨度，更新
-//         console.log('year / month changed ')
-//         imgLoad()
-//         return { message: 'year / month 图片更新啦' }
-//     }
-//     if (nowDay !== imgDay) {
-//         // 更新
-//         console.log('day changed')
-//         imgLoad()
-//         return { message: 'day 图片更新啦' }
-//     }
-//     // 没更新，静默
-//     return false
-// }
-// 十分钟检测一次 BING 是否更新了图片
-// setInterval('checkUpdate()', 10 * 60 * 1000)
-
-// 3. 调用放大图片接口
-// 3.1 保存放大图片到GitHub
-// 4. 24H循环整个流程(附带检测如果有重名的就不放大，也不保存)
