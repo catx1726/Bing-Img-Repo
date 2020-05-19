f28b8fd54a2b3a46fc4ce31dce2527e54ed8e327
catx1726 <catx1726@foxmail.com>
2020-05-19 18:35:44 +0800

    4k api 50%

--- code/js/translate.js
+++ code/js/translate.js
@@ -5,3 +5,42 @@
     定时调用 Bigjpg 的用来检测 task 是否完成的 API 
     如果完成 则保存其 图片地址 进行下载到本地，然后指定好 express 请求
 */
+
+const request = require('request')
+
+let bigJpgApi = {
+    upload,
+    download,
+}
+
+function upload(imgObj) {
+    let conf = {
+        style: 'photo',
+        noise: '1',
+        x2: '2',
+        input: imgObj.reqUrl,
+    }
+    imgObj.lock = true
+    console.log('imgObj:', imgObj)
+    request.post(
+        {
+            url: 'https://bigjpg.com/api/task/',
+            headers: { 'X-API-KEY': '684d4bdcc560497f889f895a3f726ff5' },
+            form: { conf: JSON.stringify(conf) },
+        },
+        (error, res, body) => {
+            console.error('error:', error)
+            // 当照片放大之后，会返回一个 pid 届时定时检测 pid 是否返回 true
+            imgObj.tid = body.tid
+            console.log('up api res:', body, res.statusCode)
+        }
+    )
+}
+
+function download(imgObj) {
+    // 通过 imgObj.tid 查询状态，true 就保存到服务器，然后调用 GitHub.js
+    console.log('down test')
+}
+
+console.log('bigJpgApiList:', bigJpgApi)
+module.exports = bigJpgApi
--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const request = require('request')
 const fs = require('fs')
 const path = require('path')
 const github = require('./code/js/github')
+let todayImg = {}
 
 const getImg = () => {
     console.log('getImg start.')
@@ -21,16 +22,30 @@ const getImg = () => {
                 url = img.url,
                 name = img.urlbase.split('=')[1],
                 year = img.enddate.slice(0, 4),
-                month = img.enddate.slice(4, 6)
+                month = img.enddate.slice(4, 6),
+                reqUrl = 'http://cn.bing.com' + url
 
-            request(`http://cn.bing.com${url}`).pipe(
+            request(reqUrl).pipe(
                 fs
                     .createWriteStream(
-                        path.join(__dirname,`./imgs/bing/${year}/${month}`, name + '.jpg')
+                        path.join(
+                            __dirname,
+                            `./imgs/bing/${year}/${month}`,
+                            name + '.jpg'
+                        )
                     )
                     .on('close', () => {
                         console.log(`${name} done !`)
-                        github()
+                        todayImg = {
+                            name,
+                            reqUrl,
+                            status: true,
+                            fourK: false,
+                            lock: false,
+                            tid: '',
+                        }
+                        // TODO 本地测试时，暂时关闭
+                        // github()
                     })
             )
         }
@@ -56,3 +71,19 @@ rule.minute = 0
 schedule.scheduleJob(rule, () => {
     getImg()
 })
+
+// 检查是否有资格上传为 4k
+const BigImgAPI = require('./code/js/translate')
+
+function check4KImg() {
+    // 可以上传为4K，且普通素质的图片已经下载完，且没有在处理中
+    if (todayImg.status && !todayImg.lock) {
+        BigImgAPI.upload(todayImg)
+        return true
+    }
+    if (todayImg.lock && todayImg.tid) {
+        console.log('提交接口')
+    }
+}
+
+setInterval(check4KImg, 1000)
